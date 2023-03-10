d122084f924b95aac837aa2f6abbda4e0f52857f
Nikita <hrynts2002@gmail.com>
2023-03-10 15:47:16 +0300

    Card slides finished

--- app/src/main/java/by/hryntsaliou/datingapp/adapter/DatingAdapter.kt
+++ app/src/main/java/by/hryntsaliou/datingapp/adapter/DatingAdapter.kt
@@ -1,24 +1,32 @@
 package by.hryntsaliou.datingapp.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import by.hryntsaliou.datingapp.databinding.ItemUserLayoutBinding
 import by.hryntsaliou.datingapp.model.UserModel
-import kotlinx.coroutines.NonDisposableHandle.parent
+import com.bumptech.glide.Glide
 
 class DatingAdapter(val context: Context, val list: ArrayList<UserModel>) :
     RecyclerView.Adapter<DatingAdapter.DatingViewHolder>() {
-    inner class DatingViewHolder(private val binding: ItemUserLayoutBinding) :
+    inner class DatingViewHolder(val binding: ItemUserLayoutBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DatingViewHolder {
-        TODO("Not yet implemented")
+        return DatingViewHolder(
+            ItemUserLayoutBinding.inflate(
+                LayoutInflater.from(context), parent, false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: DatingViewHolder, position: Int) {
-//        return DatingViewHolder(ItemUserLayoutBinding.bind(context),
-//        parent, false)
+
+        holder.binding.textView5.text = list[position].name
+        holder.binding.textView4.text = list[position].name
+
+        Glide.with(context).load(list[position].image).into(holder.binding.userImage)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/by/hryntsaliou/datingapp/ui/DatingFragment.kt
+++ app/src/main/java/by/hryntsaliou/datingapp/ui/DatingFragment.kt
@@ -7,15 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import by.hryntsaliou.datingapp.adapter.DatingAdapter
 import by.hryntsaliou.datingapp.databinding.FragmentDatingBinding
+import by.hryntsaliou.datingapp.model.UserModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
 
 class DatingFragment : Fragment() {
 
     private lateinit var binding: FragmentDatingBinding
+    private lateinit var manager: CardStackLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,11 +36,64 @@ class DatingFragment : Fragment() {
         return binding.root
     }
 
+    private fun init() {
+        manager = CardStackLayoutManager(requireContext(), object : CardStackListener {
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+
+                if (manager.topPosition == list.size) {
+                    Toast.makeText(
+                        requireContext(), "This is the last card",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }
+
+            override fun onCardRewound() {
+            }
+
+            override fun onCardCanceled() {
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+            }
+        })
+        manager.setVisibleCount(3)
+        manager.setTranslationInterval(0.6f)
+        manager.setScaleInterval(0.8f)
+        manager.setMaxDegree(20.0f)
+        manager.setDirections(Direction.HORIZONTAL)
+    }
+
+    private lateinit var list: ArrayList<UserModel>
     private fun getData() {
         FirebaseDatabase.getInstance().getReference("users")
             .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     Log.d("Nikita", "onDataChange: ${snapshot.toString()}")
+                    if (snapshot.exists()) {
+                        list = arrayListOf()
+                        for (data in snapshot.children) {
+                            val model = data.getValue(UserModel::class.java)
+                            list.add(model!!)
+                        }
+                        list.shuffle()
+                        init()
+                        binding.cardStackView.layoutManager = manager
+                        binding.cardStackView.itemAnimator = DefaultItemAnimator()
+                        binding.cardStackView.adapter = DatingAdapter(requireContext(), list)
+                    } else {
+                        Toast.makeText(
+                            requireContext(), "Something went wrong",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
 
                 override fun onCancelled(error: DatabaseError) {
