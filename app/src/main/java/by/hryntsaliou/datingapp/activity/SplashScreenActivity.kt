2cc66da89644bc2f68fc407eb13eba6eded16d8d
Nikita <hrynts2002@gmail.com>
2023-03-10 04:49:28 +0300

    Initial commit(Created LoginActivity and Register Activity), added Firebase support

---
+++ app/src/main/java/by/hryntsaliou/datingapp/activity/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package by.hryntsaliou.datingapp.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import by.hryntsaliou.datingapp.R
+import by.hryntsaliou.datingapp.auth.LoginActivity
+import by.hryntsaliou.datingapp.auth.RegisterActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (user == null)
+                startActivity(Intent(this, LoginActivity::class.java))
+            else
+                startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/by/hryntsaliou/datingapp/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package by.hryntsaliou.datingapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.hryntsaliou.datingapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
